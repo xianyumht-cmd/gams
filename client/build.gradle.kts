697f3c17b15cb25d5de3c5f404113defb45bf885
xianyumht-cmd <xianyumht@gmail.com>
2026-07-18 23:43:46 +0800

    Add client module build configuration

---
+++ client/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.jinli.quickweb"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jinli.quickweb"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.16.0")
+}
