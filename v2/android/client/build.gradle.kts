c936c7890accd4be8f7eabdd7de653396864486b
xianyumht-cmd <xianyumht@gmail.com>
2026-07-22 11:19:10 +0800

    Bump V2 client to test2

--- v2/android/client/build.gradle.kts
+++ v2/android/client/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.jinli.ggsecure"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "2.0.0-test1"
+        versionCode = 2
+        versionName = "2.0.0-test2"
     }
     buildTypes {
         debug { applicationIdSuffix = ".debug"; versionNameSuffix = "-debug" }
