5fffa2a8fd619ae358424b7b26104be1393faa8f
xianyumht-cmd <xianyumht@gmail.com>
2026-08-06 20:30:50 +0800

    Add diagnostic JavaScript bridge

---
+++ client_diagnostics_patch/src/main/java/com/jinli/quickweb/DiagnosticJavascriptBridge.java
@@ -0,0 +1,21 @@
+package com.jinli.quickweb;
+
+import android.webkit.JavascriptInterface;
+
+final class DiagnosticJavascriptBridge {
+    private final DiagnosticLogger logger;
+
+    DiagnosticJavascriptBridge(DiagnosticLogger logger) {
+        this.logger = logger;
+    }
+
+    @JavascriptInterface
+    public void emit(String payload) {
+        logger.logBridgePayload(payload);
+    }
+
+    @JavascriptInterface
+    public String session() {
+        return logger.sessionId();
+    }
+}
